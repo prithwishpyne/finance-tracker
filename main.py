d57c4405643f7e6cd94c687c0291aa4510a4266a
prithwishpyne <prithwishp@gmail.com>
2025-03-15 20:10:08 +0530

    CORS for deployed application

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://13.233.199.129:3000"],
+    allow_origins=["http://localhost:3000","http://65.0.139.178:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
